b50d6b773143084565dc878735e2c0fe278a2441
euiyounghwang <marieuig@gmail.com>
2024-01-27 13:33:39 -0600

    Add pytest

--- tests/test_api.py
+++ tests/test_api.py
@@ -9,6 +9,7 @@ class BaseTestAliveController(BaseTestCase):
     def test_get_restful(self):
         response = self.client.open("/api/db", method="GET")
         self.assert200(response)
+        assert response.status_code == 200
 
 
 if __name__ == "__main__":
