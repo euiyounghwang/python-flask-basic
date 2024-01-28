7d0d51ec8576efcfcea29d0d757eadccf13eebc5
euiyounghwang <marieuig@gmail.com>
2024-01-28 14:13:50 -0600

    Update

--- job/job.py
+++ job/job.py
@@ -1,13 +1,21 @@
 
 from apscheduler.schedulers.background import BackgroundScheduler
 import time
-import threading
-import asyncio
+from threading import Thread
 
 
 # Declaration of the task as a function.
 def print_date_time():
     print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+
+def thread_background():
+    while True:
+        try:
+            print('--thread_background--')
+        except Empty:
+            pass
+        time.sleep(5)  # TODO poll other things
+
 
 
 def create_jobs():
@@ -17,5 +25,8 @@ def create_jobs():
     scheduler.add_job(func=print_date_time, trigger="interval", seconds=10)
     # Start the scheduler
     scheduler.start()
+    
+    # Create thread as background
+    Thread(target=thread_background, daemon=True).start()
     
   
