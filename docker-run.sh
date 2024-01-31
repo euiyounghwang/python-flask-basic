9cacc6b858796826632865dfaa5e7b96d6ec93d7
euiyounghwang <marieuig@gmail.com>
2024-01-31 16:44:17 -0600

    Update

--- docker-run.sh
+++ docker-run.sh
@@ -12,6 +12,8 @@ docker run --rm --platform linux/amd64 -it -d \
   --name fn-flask-basic-pi --publish 15000:5000 --expose 5000 \
   --network bridge \
   -e DATABASE_URL=postgresql://postgres:1234@host.docker.internal:15432/postgres \
+  -e ES_HOST=http://host.docker.internal:9209 \
+  -e ES_LOGGER_INDEX=kafka_indexing \
   -e KAFKA_HOST=host.docker.internal:29092,host.docker.internal:39092 \
   -e KAFKA_TOPIC=test-topic,test1-topic \
   -v "$SCRIPTDIR:/app/FN-BEES-Services/" \
--- docker-test.sh
+++ docker-test.sh
@@ -11,6 +11,8 @@ SCRIPTDIR="$(
 # VS Code : Remove -it
 docker run --rm -it --name euiyoung/fn-flask-basic-api-test --publish 15001:5000 --expose 5000 \
   -e DATABASE_URL=postgresql://postgres:1234@host.docker.internal:15432/postgres \
+  -e ES_HOST=http://host.docker.internal:9209 \
+  -e ES_LOGGER_INDEX=kafka_indexing \
   -e KAFKA_HOST=host.docker.internal:29092,host.docker.internal:39092 \
   -e KAFKA_TOPIC=test-topic,test1-topic \
   -v "$SCRIPTDIR:/app/FN-BEES-Services/" \
